263dc47b5efc9256a5ad284ab97c1aa216cc2cbd
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2012-11-16 10:45:16 -0800

    Restore ZMQ as default implementation.

--- src/SocketFactory.cpp
+++ src/SocketFactory.cpp
@@ -17,19 +17,23 @@ SocketFactory::~SocketFactory(){
 }
 
 ISocket *SocketFactory::createClientSocket(Channel channel, Semantics semantics, void (*deallocator)(void *, void *)){
-  if(channel.context == LOCAL){
+  return m_zmqFactory->createClientSocket(channel, semantics, deallocator);
+
+  /*if(channel.context == LOCAL){
     return m_pipeFactory->createClientSocket(channel, semantics, deallocator);
   }else{
     return m_zmqFactory->createClientSocket(channel, semantics, deallocator);
-  }
+  }*/
 }
 
 ISocket *SocketFactory::createServerSocket(Channel channel, Semantics semantics, void (*deallocator)(void *, void *)){
-  if(channel.context == LOCAL){
+  return m_zmqFactory->createServerSocket(channel, semantics, deallocator);
+
+  /*if(channel.context == LOCAL){
     return m_pipeFactory->createServerSocket(channel, semantics, deallocator);
   }else{
     return m_zmqFactory->createServerSocket(channel, semantics, deallocator);
-  }
+  }*/
 }
 
 
