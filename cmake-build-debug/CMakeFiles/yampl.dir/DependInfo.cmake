3039a5a7b2c80b7b22556cae82353a41cb3ae79a
Ayoub Chouak <a.chouak@protonmail.com>
2018-04-29 18:20:44 +0200

    Edited gitignore

---
+++ cmake-build-debug/CMakeFiles/yampl.dir/DependInfo.cmake
@@ -0,0 +1,92 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/src/ISocket.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/src/ISocket.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/src/SocketFactory.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/src/SocketFactory.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/src/pipe/PipeSocketBase.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/src/pipe/PipeSocketBase.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/src/pipe/PipeSocketFactory.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/src/pipe/PipeSocketFactory.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/src/plugin/PluginArbiter.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/src/plugin/PluginArbiter.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/src/zeromq/ClientSocket.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/src/zeromq/ClientSocket.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/src/zeromq/ServerSocket.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/src/zeromq/ServerSocket.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/src/zeromq/SocketBase.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/src/zeromq/SocketBase.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/src/zeromq/SocketFactory.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/src/zeromq/SocketFactory.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/address.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/address.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/clock.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/clock.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/ctx.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/ctx.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/dealer.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/dealer.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/decoder.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/decoder.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/devpoll.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/devpoll.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/dist.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/dist.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/encoder.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/encoder.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/epoll.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/epoll.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/err.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/err.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/fq.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/fq.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/io_object.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/io_object.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/io_thread.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/io_thread.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/ip.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/ip.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/ipc_address.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/ipc_address.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/ipc_connecter.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/ipc_connecter.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/ipc_listener.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/ipc_listener.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/kqueue.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/kqueue.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/lb.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/lb.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/mailbox.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/mailbox.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/msg.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/msg.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/mtrie.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/mtrie.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/object.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/object.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/options.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/options.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/own.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/own.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/pair.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/pair.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/pgm_receiver.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/pgm_receiver.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/pgm_sender.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/pgm_sender.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/pgm_socket.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/pgm_socket.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/pipe.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/pipe.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/poll.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/poll.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/poller_base.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/poller_base.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/precompiled.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/precompiled.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/proxy.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/proxy.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/pub.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/pub.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/pull.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/pull.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/push.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/push.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/random.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/random.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/reaper.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/reaper.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/rep.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/rep.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/req.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/req.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/router.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/router.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/select.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/select.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/session_base.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/session_base.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/signaler.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/signaler.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/socket_base.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/socket_base.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/stream_engine.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/stream_engine.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/sub.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/sub.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/tcp.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/tcp.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/tcp_address.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/tcp_address.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/tcp_connecter.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/tcp_connecter.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/tcp_listener.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/tcp_listener.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/thread.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/thread.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/trie.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/trie.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/v1_decoder.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/v1_decoder.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/v1_encoder.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/v1_encoder.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/xpub.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/xpub.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/xsub.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/xsub.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/zmq.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/zmq.cpp.o"
+  "/home/ntauthority/Desktop/CERN-HSF/yampl/zeromq/src/zmq_utils.cpp" "/home/ntauthority/Desktop/CERN-HSF/yampl/cmake-build-debug/CMakeFiles/yampl.dir/zeromq/src/zmq_utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../zeromq/include"
+  "/usr/local/include"
+  "/usr/local/share/libsourcey/vendor/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
