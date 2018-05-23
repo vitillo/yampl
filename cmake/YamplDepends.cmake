d30374d8a90e6d39b954fe5edbd2450a34d13865
Ayoub Chouak <a.chouak@protonmail.com>
2018-05-24 01:12:59 +0200

    Fixed several issues (file permissions, directory independent builds, install prefix environment variable)

--- cmake/YamplDepends.cmake
+++ cmake/YamplDepends.cmake
@@ -3,26 +3,26 @@ include(ExternalProject)
 ###############################################################################
 # LibSourcey
 ###############################################################################
-ExternalProject_Add(
-        LibSourcey
-
-        GIT_REPOSITORY "https://github.com/sourcey/libsourcey.git"
-        GIT_TAG "1.1.4"
-        UPDATE_COMMAND ""
-        PATCH_COMMAND ""
-
-        SOURCE_DIR "libscy"
-        CMAKE_ARGS -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_APPLICATIONS=OFF -DBUILD_TESTS=OFF -DBUILD_SAMPLES=OFF -DBUILD_MODULES=OFF -DBUILD_MODULE_base=ON -DBUILD_MODULE_uv=ON -DBUILD_MODULE_pluga=ON -DWITH_OPENCV=OFF -DWITH_WEBRTC=OFF -DWITH_FFMPEG=OFF
-
-        TEST_COMMAND ""
-)
+#ExternalProject_Add(
+#        LibSourcey
+#
+#        GIT_REPOSITORY "https://github.com/sourcey/libsourcey.git"
+#        GIT_TAG "1.1.4"
+#        UPDATE_COMMAND ""
+#        PATCH_COMMAND ""
+#
+#        SOURCE_DIR "libscy"
+#        CMAKE_ARGS -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_APPLICATIONS=OFF -DBUILD_TESTS=OFF -DBUILD_SAMPLES=OFF -DBUILD_MODULES=OFF -DBUILD_MODULE_base=ON -DBUILD_MODULE_uv=ON -DBUILD_MODULE_pluga=ON -DWITH_OPENCV=OFF -DWITH_WEBRTC=OFF -DWITH_FFMPEG=OFF
+#
+#        TEST_COMMAND ""
+#)
 
 ###############################################################################
 # Pluga
 ###############################################################################
-ExternalProject_Add_Step(
-        LibSourcey AddPluga
-        COMMAND git clone https://github.com/sourcey/pluga.git pluga
-        COMMAND mv pluga libscy/src/
-        DEPENDEES download
-)
+#ExternalProject_Add_Step(
+#        LibSourcey AddPluga
+#        COMMAND git clone https://github.com/sourcey/pluga.git pluga
+#        COMMAND mv pluga libscy/src/
+#        DEPENDEES download
+#)
