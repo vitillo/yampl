d9b68b356249d507e9006bbaf7978473dcbbbe79
Ayoub Chouak <a.chouak@protonmail.com>
2018-05-26 02:06:29 +0200

    Edited PluginArbiter

--- src/plugin/PluginArbiter.cpp
+++ src/plugin/PluginArbiter.cpp
@@ -240,6 +240,21 @@ namespace yampl
 
         }
 
+        PluginArbiter::Handle::Handle(Handle const& rhs) noexcept {
+
+        }
+
+        PluginArbiter::Handle::Handle(Handle&& rhs) noexcept
+            : _moniker(std::move(rhs._moniker))
+            , _handle_id(rhs._handle_id)
+            , _arbiter(std::move(rhs._arbiter))
+            , _obj_alloc_list(std::move(rhs._obj_alloc_list))
+            , _obj_type_map(std::move(rhs._obj_type_map))
+        {
+            // Invalidate the handle id of the moved handle
+            rhs._handle_id = _handle_id_invalid;
+        }
+
         void PluginArbiter::Handle::destroy_object(IObject* obj) const
         {
             try
