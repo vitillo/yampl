28e3c336a0fec01d83795b5f5e7cb4a5a4913165
Ayoub Chouak <a.chouak@protonmail.com>
2018-06-08 18:35:31 +0200

    Re-added plugins

---
+++ plugins/yampl-shm/yampl-shm.conf.cmake
@@ -0,0 +1,21 @@
+# YAMPL-SHM
+if (NOT DEFINED WITH_PLUGIN_SHM OR WITH_PLUGIN_SHM)
+    add_library(yampl-shm SHARED
+            ${CMAKE_CURRENT_LIST_DIR}/src/ServerSocket.cpp
+            ${CMAKE_CURRENT_LIST_DIR}/src/SHMSocketBase.cpp
+            ${CMAKE_CURRENT_LIST_DIR}/src/SHMSocketFactory.cpp
+            ${CMAKE_CURRENT_LIST_DIR}/src/PluginMain.cpp
+            ${YAMPL_PLUGIN_COMMON_SRCS}
+    )
+
+    target_include_directories(yampl-shm PRIVATE ${CMAKE_CURRENT_LIST_DIR}/include)
+
+    set_target_properties(yampl-shm
+            PROPERTIES
+            LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/plugins/yampl-shm"
+    )
+
+    install(TARGETS yampl-shm
+            LIBRARY DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/plugins
+    )
+endif()
